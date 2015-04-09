f741bd746ca2ac46fa59fc18addea38158292043
Jmeas <jellyes2@gmail.com>
2015-04-08 23:16:26 -0400

    Move source code around. Clean up API.

--- src/surfaces.js
+++ src/surfaces.js
@@ -3,6 +3,8 @@ import twoRotations from 'two-rotations';
 import compute from './compute';
 import createPlane from './create-plane';
 
+// The options that can be passed into
+// a new Surface instance
 var surfaceOptions = [
   'tagName', 'fn', 'el',
   'width', 'height',
@@ -12,12 +14,9 @@ var surfaceOptions = [
   'currentFrame', 'maxPitch',
 ];
 
-// Quick-round to the nearest integer
-function round(somenum) {
-  return Math.round(somenum);
-}
-
 class Surface {
+
+  // Create a new Surface
   constructor(options) {
     _.defaults(this, _.pick(options, surfaceOptions), {
       width: 300,
@@ -42,44 +41,17 @@ class Surface {
     this._generateRotation();
 
     this._ensureElement();
+    this._setType();
     this.initialize(...arguments);
   }
 
+  // A method that can be overwritten to augment
+  // the instantiation of a Surface
   initialize() {}
 
-  _generateRotation() {
-    this._rotationMatrix = twoRotations.generateMatrix(this.yaw, this.pitch);
-  }
-
-  _ensureElement() {
-    if (!this.el) {
-      this.el = this._createElement(_.result(this, 'tagName'));
-    } else {
-      this.el = _.result(this, 'el');
-    }
-
-    if (this.el.nodeName.toLowerCase() === 'canvas') {
-      this.el.width = this.width;
-      this.el.height = this.height;
-    }
-  }
-
-  _computeData() {
-    return compute(...arguments);
-  }
-
-  _createElement(tagName) {
-    if (tagName === 'svg') {
-      return document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    } else if (tagName === 'canvas') {
-      return document.createElement('canvas');
-    } else {
-      throw new Error('The element must be a canvas or svg.');
-    }
-  }
-
+  // Generate, and cache, our data
   computeData() {
-    this._cache = this._computeData({
+    this._cache = compute({
       fn: this.fn,
       startTime: this.currentFrame,
       maxTime: this.currentFrame,
@@ -91,6 +63,7 @@ class Surface {
     return this;
   }
 
+  // Adjust the orientation of the Surface
   orient(orientation) {
     _.extend(this, _.pick(orientation, ['yaw', 'pitch']));
 
@@ -100,6 +73,7 @@ class Surface {
     return this;
   }
 
+  // Render the surface into its element
   render() {
     if (!this._cache) {
       this.computeData();
@@ -120,21 +94,60 @@ class Surface {
     return this;
   }
 
+  // Set our rotation matrix
+  _generateRotation() {
+    this._rotationMatrix = twoRotations.generateMatrix(this.yaw, this.pitch);
+  }
+
+  // Make sure that this Surface has an
+  // associated SVG or Canvas element
+  _ensureElement() {
+    if (!this.el) {
+      this.el = this._createElement(_.result(this, 'tagName'));
+    } else {
+      this.el = _.result(this, 'el');
+    }
+
+    if (this.el.nodeName.toLowerCase() === 'canvas') {
+      this.el.width = this.width;
+      this.el.height = this.height;
+    }
+  }
+
+  // Set whether this Surface is an SVG or a Canvas
+  _setType() {
+    this._type = this.el.nodeName.toLowerCase();
+  }
+
+  // Create a new element, given a tagName
+  _createElement(tagName) {
+    if (tagName === 'svg') {
+      return document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    } else if (tagName === 'canvas') {
+      return document.createElement('canvas');
+    } else {
+      throw new Error('The element must be a canvas or svg.');
+    }
+  }
+
+  // Render a surface as a Canvas
   _renderCanvasSurface(plane) {
     var context = this.el.getContext('2d');
 
     // Clear the canvas
     context.clearRect (0, 0, this.el.width, this.el.height);
+
+    // Loop through our data, drawing each piece of the surface
     var p;
     plane.forEach(a => {
       p = a.path;
       context.beginPath();
       context.fillStyle = this.colorFn(a.avg);
       context.strokeStyle = 'rgba(0, 0, 0, 0.4)';
-      context.moveTo(round(p.moveTo[0]), round(p.moveTo[1]));
-      context.lineTo(round(p.pointOne[0]), round(p.pointOne[1]));
-      context.lineTo(round(p.pointTwo[0]), round(p.pointTwo[1]));
-      context.lineTo(round(p.pointThree[0]), round(p.pointThree[1]));
+      context.moveTo(Math.round(p.moveTo[0]), Math.round(p.moveTo[1]));
+      context.lineTo(Math.round(p.pointOne[0]), Math.round(p.pointOne[1]));
+      context.lineTo(Math.round(p.pointTwo[0]), Math.round(p.pointTwo[1]));
+      context.lineTo(Math.round(p.pointThree[0]), Math.round(p.pointThree[1]));
       context.stroke();
       context.fill();
     });
